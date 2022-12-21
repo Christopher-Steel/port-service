e250e53582a7bb9fb7a0db3d5232e39ca1763d63
Chris Steel <christopher.steel@helsing.ai>
2022-12-21 17:05:23 +0000

    First go at a very simple db abstraction

---
+++ internal/db/db-wrapper.go
@@ -0,0 +1,21 @@
+package db
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type Database struct {
+	db *gorm.DB
+}
+
+var dbConnection *Database
+
+func connect() error {
+	db, err := gorm.Open(sqlite.Open("port.db"), &gorm.Config{})
+	if err != nil {
+		return err
+	}
+	dbConnection = &Database{db: db}
+	return nil
+}
